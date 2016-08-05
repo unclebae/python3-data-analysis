14933aee7b94ac8b8225ecc4a6aa76d62dd80aa6
kido(UncleBae) <kido@coupang.com>
2016-08-05 13:56:24 +0900

    satistical method

---
+++ ch03/statisticsNumpy.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+import numpy as np
+from scipy.stats import scoreatpercentile
+
+data = np.loadtxt("test_bundle.csv", delimiter=',', usecols=(1,), skiprows=1, unpack=True)
+
+print ("Max method", data.max())
+print ("Max function", np.max(data))
+
+print ("Min method", data.min())
+print ("Min function", np.min(data))
+
+print ("Mean method", data.mean())
+print ("Mean function", np.mean(data))
+
+print ("Std method", data.std())
+print ("std function", np.std(data))
+
+print ("Median", np.median(data))
+print ("Score at percentile 50", scoreatpercentile(data, 50))
+
